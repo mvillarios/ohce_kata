8b54d9cde12382564a32e5b2ebd000da259d3109
miosv <mvilla2019@inf.udec.cl>
2023-08-28 19:58:24 -0400

    elimino test 2 (innecesario)

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 from app.ohce import Ohce
 
-def test_nombre():
-    assert Ohce("Pedro") == "Pedro"
+# def test_nombre():
+#     assert Ohce("Pedro") == "Pedro"
 
 def test_saludo():
     assert Ohce("Pedro") == "¡Buenos días Pedro!"
