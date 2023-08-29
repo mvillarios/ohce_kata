0d55f894def33f81fb4b3051d8ef8dc284afd5cb
miosv <mvilla2019@inf.udec.cl>
2023-08-28 20:12:13 -0400

    agrego test 4 fallido (saludo segun hora especifica)

--- test.py
+++ test.py
@@ -1,7 +1,15 @@
 from app.ohce import Ohce
+from datetime import datetime
 
 # def test_nombre():
 #     assert Ohce("Pedro") == "Pedro"
 
-def test_saludo():
-    assert Ohce("Pedro") == "¡Buenos días Pedro!"
+def test_saludo_hora():
+    #Test de saludo según la hora
+    hora = datetime.now().hour
+    if hora >= 6 and hora < 12 :
+        assert Ohce("Pedro") == "¡Buenos días Pedro!" # 6:00 - 12:00
+    elif hora >= 12 and hora < 20:
+        assert Ohce("Pedro") == "¡Buenas tardes Pedro!" # 12:00 - 20:00
+    else:
+        assert Ohce("Pedro") == "¡Buenas noches Pedro!" # 20:00 - 6:00
