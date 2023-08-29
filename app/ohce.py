880ac7d0eef4b52bf6667412f81413e4d9a61231
miosv <mvilla2019@inf.udec.cl>
2023-08-28 20:13:54 -0400

    cambio para pasar test 4

--- app/ohce.py
+++ app/ohce.py
@@ -1,2 +1,10 @@
+from datetime import datetime
+
 def Ohce(nombre):
-    return "¡Buenos días " + nombre + "!"
+    hora = datetime.now().hour
+    if hora >= 6 and hora < 12 :
+        return "¡Buenos días " + nombre + "!"
+    elif hora >= 12 and hora < 20:
+        return "¡Buenas tardes " + nombre + "!"
+    else:
+        return "¡Buenas noches " + nombre + "!"
