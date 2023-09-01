ea353528f9b86d93468ec6e2bf98ddc8cc36d220
miosv <mvilla2019@inf.udec.cl>
2023-09-01 14:11:35 -0400

    cambios para que funcione en consolo y con prints los tests, ademas pasa el test_adios

--- app/ohce.py
+++ app/ohce.py
@@ -1,20 +1,48 @@
 from datetime import datetime
 
-def Ohce(string):
+# def Ohce(string):
+#     if string == "Stop!":
+      
+#     if "ohce" in string:
+#         string = string.replace("ohce ", "")
+#         hora = datetime.now().hour
+#         if hora >= 6 and hora < 12 :
+#             return "¡Buenos días " + string + "!"
+#         elif hora >= 12 and hora < 20:
+#             return "¡Buenas tardes " + string + "!"
+#         else:
+#             return "¡Buenas noches " + string + "!"
+#     else:
+#         if es_palindromo(string):
+#             return string + "\n" +"¡Bonita palabra!"
+#         else:
+#             return string[::-1]
+        
+
+def Ohce():
+    nombre = ""
+    string = input()
     if "ohce" in string:
         string = string.replace("ohce ", "")
+        nombre = string
         hora = datetime.now().hour
         if hora >= 6 and hora < 12 :
-            return "¡Buenos días " + string + "!"
+            print("¡Buenos días " + string + "!")
         elif hora >= 12 and hora < 20:
-            return "¡Buenas tardes " + string + "!"
+            print("¡Buenas tardes " + string + "!")
         else:
-            return "¡Buenas noches " + string + "!"
+            print("¡Buenas noches " + string + "!")
+    elif string == "Stop!":
+        print("Adios " + nombre)
     else:
         if es_palindromo(string):
-            return string + "\n" +"¡Bonita palabra!"
+            print(string + "\n" +"¡Bonita palabra!")
         else:
-            return string[::-1]
+            print(string[::-1])
     
+
 def es_palindromo(string):
     return string == string[::-1]
+
+if __name__ == "__main__":
+    Ohce()
--- test.py
+++ test.py
@@ -1,33 +1,47 @@
+import pytest
 from app.ohce import Ohce
 from datetime import datetime
 from app.ohce import es_palindromo
+from unittest.mock import patch
 
-# def test_nombre():
-#     assert Ohce("Pedro") == "Pedro"
-
-def test_saludo_hora_inicio():
-    # Test de saludo según la hora solo cuando se inicia el programa con ohce
+def test_saludo_hora_inicio(capsys):
     hora = datetime.now().hour
     if hora >= 6 and hora < 12 :
-        assert Ohce("ohce Pedro") == "¡Buenos días Pedro!" # 6:00 - 12:00
+        expected_output = "¡Buenos días Pedro!"
     elif hora >= 12 and hora < 20:
-        assert Ohce("ohce Pedro") == "¡Buenas tardes Pedro!" # 12:00 - 20:00
+        expected_output = "¡Buenas tardes Pedro!"
     else:
-        assert Ohce("ohce Pedro") == "¡Buenas noches Pedro!" # 20:00 - 6:00
+        expected_output = "¡Buenas noches Pedro!"
+    
+    with patch("builtins.input", return_value="ohce Pedro"):
+        Ohce()
+    
+    captured = capsys.readouterr()
+    assert captured.out.strip() == expected_output
 
-def test_echo():
-    # Test de echo
-    assert Ohce("hola") == "aloh"
+def test_echo(capsys):
+    with patch("builtins.input", return_value="hola"):
+        Ohce()
+    
+    captured = capsys.readouterr()
+    assert captured.out.strip() == "aloh"
 
-def test_palindromo():
-    # Test de palíndromo
+def test_palindromo(capsys):
     assert es_palindromo("oto") == True
 
-def test_bonita_palabra():
-    # Test de bonita palabra
+def test_bonita_palabra(capsys):
     palabra = "oto"
-    assert Ohce(palabra) == palabra + "\n" +"¡Bonita palabra!"
+    with patch("builtins.input", return_value=palabra):
+        Ohce()
+    
+    expected_output = f"{palabra}\n¡Bonita palabra!"
+    captured = capsys.readouterr()
+    assert captured.out.strip() == expected_output
 
-def test_adios():
-    # Test de adiós
-    assert Ohce("Stop!") == "Adios Pedro"
+def test_adios(capsys):
+    with patch("builtins.input", side_effect=["Stop!"]):
+        Ohce()
+    
+    captured = capsys.readouterr()
+    expected_output = "Adios"
+    assert captured.out.strip() == expected_output
